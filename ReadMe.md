23c3649898adbebc1e0632b2b0303f8e77b14404
Nicu Tofan <nicu.tofan@gmail.com>
2015-04-22 20:03:40 +0300

    Initial commit

---
+++ ReadMe.md
@@ -0,0 +1,30 @@
+Pylearn2 Extra
+--------------
+
+This repository contains code that extends the [Pylearn2][1] library,
+a Machine Learning library based on [Theano][2].
+
+The structure of the code tries to mimic that of the
+[Pylearn2][1] library. Some support files (
+LICENSE.txt, MANIFEST.in, setup.py, etc) were reused.
+
+[Pylearn2][1] was also added as a submodule, so you can:
+
+    git clone https://github.com/TNick/pyl2extra.git
+    cd pyl2extra
+    git submodule update --init
+
+The setup.py script will look for pylearn2 directory and, if present,
+will call it just like you would:
+
+    cd pylearn2
+    python setup.py develop --user
+
+Please note that I am in no way affiliated with
+the folks at [Lisa lab][3] that developed [Pylearn2][1] library.
+
+
+
+  [1]: https://github.com/lisa-lab/pylearn2 "Pylearn2 GitHub repository"
+  [2]: https://github.com/Theano/Theano "Theano - define, optimize, and evaluate mathematical expressions"
+  [3]: http://www.iro.umontreal.ca/~lisa/ "Laboratoire d’Informatique des Systèmes Adaptatifs"
---
+++ setup.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+"""
+This module is the centre of all activity in building, distributing,
+and installing using the Distutils.
+"""
+
+from __future__ import print_function
+
+from setuptools import setup, find_packages
+from setuptools.command.install import install
+
+import os
+import sys
+import subprocess
+
+if sys.version_info > (3, 0):
+    ask_user = input
+else:
+    ask_user = raw_input
+
+cmdclass = {}
+ext_modules = []
+base_path = os.path.split(os.path.abspath(__file__))[0]
+
+class Pyl2ExtraInstall(install):
+    """
+    Customize install process.
+    """
+    def run(self):
+        global base_path
+        
+        if os.path.isdir(os.path.join(base_path, 'pylearn2')):
+            crt_dir = os.getcwd()
+            os.chdir(os.path.join(base_path, 'pylearn2'))
+            subprocess.call(['python', 'setup.py', 'install'])
+            os.chdir(crt_dir)
+
+        # allow for unattended install (pylearn2 still needs input)
+        try:
+            mode = os.environ['PYL2EXTRA_INSTALL_MODE']
+        except KeyError:
+            mode = None
+
+        while mode not in ['', 'install', 'develop', 'cancel']:
+            if mode is not None:
+                print("Please try again")
+            mode = ask_user("pyl2extra installation mode: [develop]/install/cancel: ")
+        if mode in ['', 'develop']:
+            self.distribution.run_command('develop')
+        if mode == 'install':
+            return install.run(self)
+cmdclass.update({'install': Pyl2ExtraInstall})
+
+setup(
+    cmdclass=cmdclass,
+    ext_modules=ext_modules,
+    name='pyl2extra',
+    version='0.1dev',
+    packages=find_packages(),
+    description='Extends pylearn2 library.',
+    license='BSD 3-clause license',
+    long_description=open('ReadMe.md', 'rb').read().decode('utf8'),
+    dependency_links=['git+http://github.com/Theano/Theano.git#egg=Theano'],
+    install_requires=['numpy>=1.5', 'pyyaml', 'argparse', "Theano"],
+    scripts=[],
+    package_data={
+        '': ['*.cu', '*.cuh', '*.h'],
+    },
+)
