0c337ef4f083b101e9b44eee5307d1360b45f218
Nicu Tofan <nicu.tofan@gmail.com>
2015-05-19 23:31:17 +0300

    cos_dataset tests updated

--- pyl2extra/datasets/tests/test_cos_dataset.py
+++ pyl2extra/datasets/tests/test_cos_dataset.py
@@ -5,7 +5,6 @@ from pyl2extra.datasets.cos_dataset import CosDataset, _banned_mods
 from pylearn2.utils.iteration import _iteration_schemes
 from pylearn2.utils.rng import make_np_rng
 from pylearn2.space import VectorSpace
-import numpy as np
 from nose.tools import assert_raises
 
 from itertools import chain
@@ -17,54 +16,86 @@ def test_constructor():
     
 def test_iterator():
     
-    cdset = CosDataset()
+    std_dev = 0.005
+    cdset = CosDataset(std=std_dev)
     
-    itr = cdset.iterator()
     indiv_ex = range(100)
-    std_dev = 0.05
     cklim = 1 + std_dev*6
     def is_in_valid_range(y):
-        return np.all(y < cklim and y > -cklim)
+        result = True
+        for y1 in y:
+            for y2 in y1:
+                result = result and y2 < cklim and y2 > -cklim
+                #print result, -cklim, y2, cklim
+                
+        #result = np.all([np.all(yval < cklim and yval > -cklim) for yval in y])
+        #print zip(y, [np.all(yval < cklim and yval > -cklim) for yval in y])        
+        return result
     
+    itr = cdset.iterator()
     for i in indiv_ex:
-        y = itr.next()
+        try:
+            y = itr.next()
+        except StopIteration:
+            itr = cdset.iterator()
+            y = itr.next()
         #print y, list(y <= 1.05 and y >= -1.05)
         assert is_in_valid_range(y)
 
     for imode in chain(_iteration_schemes, ['random']):
-        print imode, imode in _banned_mods, _banned_mods
+        #print imode, imode in _banned_mods, _banned_mods
         if imode in _banned_mods:
             assert_raises(ValueError, cdset.iterator, mode=imode)
         else:
             itr = cdset.iterator(mode=imode)
             for i in indiv_ex:
-                y = itr.next()
+                try:
+                    y = itr.next()
+                except StopIteration:
+                    itr = cdset.iterator(mode=imode)
+                    y = itr.next()
                 assert is_in_valid_range(y)
     
     for batchsz in [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 4096]:
         print batchsz
         itr = cdset.iterator(batch_size=batchsz)
         for i in indiv_ex:
-            y = itr.next()
+            try:
+                y = itr.next()
+            except StopIteration:
+                itr = cdset.iterator(batch_size=batchsz)
+                y = itr.next()
+                
             assert is_in_valid_range(y)
         
     rng = make_np_rng((1, 2, 3), which_method=['uniform', 'randn'])
     itr = cdset.iterator(mode='random', rng=rng)
     for i in indiv_ex:
-        y = itr.next()
+        try:
+            y = itr.next()
+        except StopIteration:
+            itr = cdset.iterator(mode='random', rng=rng)
+            y = itr.next()
         assert is_in_valid_range(y)
         
-    data_specs = (VectorSpace(2), 'source')
+    data_specs = (VectorSpace(2), 'features')
     itr = cdset.iterator(data_specs=data_specs)
     for i in indiv_ex:
-        y = itr.next()
+        try:
+            y = itr.next()
+        except StopIteration:
+            itr = cdset.iterator(data_specs=data_specs)
+            y = itr.next()
         assert is_in_valid_range(y)
         
     itr = cdset.iterator(return_tuple=True)
     for i in indiv_ex:
-        y = itr.next()
+        try:
+            y = itr.next()
+        except StopIteration:
+            itr = cdset.iterator(return_tuple=True)
+            y = itr.next()
         assert isinstance(y, tuple)
-        #print y
-        
-    #assert False
     
+if __name__ == "__main__":
+    test_iterator()
