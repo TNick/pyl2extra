4f8608ea8e8d01e5b1429237c5bbbdd14e6565f2
Nicu Tofan <nicu.tofan@gmail.com>
2015-05-24 20:18:35 +0300

    Cosmetics

--- pyl2extra/datasets/img_dataset/tests/test_dataset.py
+++ pyl2extra/datasets/img_dataset/tests/test_dataset.py
@@ -2,6 +2,13 @@
 Tests for dataset
 """
 
+__authors__ = "Nicu Tofan"
+__copyright__ = "Copyright 2015, Nicu Tofan"
+__credits__ = ["Nicu Tofan"]
+__license__ = "3-clause BSD"
+__maintainer__ = "Nicu Tofan"
+__email__ = "nicu.tofan@gmail.com"
+
 import functools
 import Image
 import logging
