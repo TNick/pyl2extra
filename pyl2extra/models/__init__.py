35154313a96dd343202866d378863e6b0ad58a6c
Nicu Tofan <nicu.tofan@gmail.com>
2015-06-20 01:05:18 +0300

    Proper module for models

---
+++ pyl2extra/models/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Models and layer compatible with pylearn2's format.
+"""
+
+__authors__ = "Nicu Tofan"
+__copyright__ = "Copyright 2015, Nicu Tofan"
+__credits__ = ['Nicu Tofan']
+__license__ = "3-clause BSD"
+__maintainer__ = "Nicu Tofan"
+__email__ = "nicu.tofan@gmail.com"
