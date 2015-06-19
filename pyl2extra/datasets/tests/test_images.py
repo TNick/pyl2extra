7d65604d93351759279a196be8024045fd802873
Nicu Tofan <nicu.tofan@gmail.com>
2015-06-20 01:05:39 +0300

    Factor out image creation

--- pyl2extra/datasets/tests/test_images.py
+++ pyl2extra/datasets/tests/test_images.py
@@ -11,80 +11,13 @@ __email__ = "nicu.tofan@gmail.com"
 
 import csv
 import functools
-import Image
-import numpy
 import os
 import shutil
 import tempfile
 import unittest
-from collections import OrderedDict
-
 
 from pyl2extra.datasets.images import Images
-
-
-def create_images(path, factor):
-    """
-    Creates a number of images for testing using various encodings.
-
-    The result is a dictionary with keys being file names and values
-    being  tuples of (category, image).
-
-    TODO: this is duplicated code from
-    `pyl2extra/datasets/img_dataset/tests/test_data_providers.py`
-    """
-    result = OrderedDict()
-
-    for i in range(factor):
-        img_file = os.path.join(path, 'rgba_file_%d.png' % i)
-        imarray = numpy.random.rand(100,100,3) * 255
-        im = Image.fromarray(imarray.astype('uint8')).convert('RGBA')
-        im.save(img_file)
-        result[img_file] = ('rgba', im, 0)
-
-        img_file = os.path.join(path, 'rgb_file_%d.png' % i)
-        imarray = numpy.random.rand(100,50,3) * 255
-        im = Image.fromarray(imarray.astype('uint8')).convert('RGB')
-        im.save(img_file)
-        result[img_file] = ('rgb', im, 1)
-
-        img_file = os.path.join(path, 'greyscale_file_%d.png' % i)
-        imarray = numpy.random.rand(50,100,3) * 255
-        im = Image.fromarray(imarray.astype('uint8')).convert('L')
-        im.save(img_file)
-        result[img_file] = ('l', im, 2)
-
-        img_file = os.path.join(path, 'black_white_file_%d.png' % i)
-        imarray = numpy.random.rand(100,10,3) * 2
-        im = Image.fromarray(imarray.astype('uint8')).convert('1')
-        im.save(img_file)
-        result[img_file] = ('bw', im, 3)
-
-        img_file = os.path.join(path, 'rpalette_file_%d.png' % i)
-        imarray = numpy.random.rand(10,100,3) * 255
-        im = Image.fromarray(imarray.astype('uint8')).convert('P')
-        im.save(img_file)
-        result[img_file] = ('palette', im, 4)
-
-        img_file = os.path.join(path, 'cmyk_file_%d.jpg' % i)
-        imarray = numpy.random.rand(255,254,3) * 255
-        im = Image.fromarray(imarray.astype('uint8')).convert('CMYK')
-        im.save(img_file)
-        result[img_file] = ('cmyk', im, 5)
-
-        img_file = os.path.join(path, 'integer_file_%d.png' % i)
-        imarray = numpy.random.rand(10,11,3) * 255
-        im = Image.fromarray(imarray.astype('uint8')).convert('I')
-        im.save(img_file)
-        result[img_file] = ('integer', im, 6)
-
-        img_file = os.path.join(path, 'float_file_%d.tif' % i)
-        imarray = numpy.random.rand(999,999,3) * 255
-        im = Image.fromarray(imarray.astype('uint8')).convert('F')
-        im.save(img_file)
-        result[img_file] = ('float', im, 7)
-
-    return result
+from pyl2extra.testing import images
 
 
 class TestGeneric(unittest.TestCase):
@@ -95,7 +28,7 @@ class TestGeneric(unittest.TestCase):
     def setUp(self):
         self.testee = None
         self.tmp_dir = tempfile.mkdtemp()
-        self.testset = create_images(self.tmp_dir, 10)
+        self.testset = images.create(self.tmp_dir, 10)
         self.csv_file_fc = os.path.join(self.tmp_dir, 'files_and_categs.csv')
         with open(self.csv_file_fc, 'wt') as fhand:
             csvw = csv.writer(fhand, delimiter=',', quotechar='"')
